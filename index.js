71535025c976cbc57c8e8ca3a7c98c4dc4e7248b
HaykoKoryun <haykokoryun@gmail.com>
2019-06-20 05:42:22 +0400

    initial working version

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
 require('dotenv').config();
+const fs = require('fs');
+
+const lots = process.env.LOTS;
 
 const slack = require('./slack');
 
 (async () =>
 { await slack.init();
+
+  for(let i = 1; i <= lots; ++i)
+  { await auctionLot(i);
+  }
 })();
+
+const auctionLot = async (lot) =>
+{ const padded = `${lot}`.padStart(3, '0');
+  const info = fs.readFileSync(`lots/${padded}/info.txt`, 'utf8');
+  const tokens = info.split('\n');
+
+  const [photo, minimum] = tokens[0].split(':');
+
+  await slack.postLotInGeneral(padded, photo);
+  await slack.postLot(padded, photo);
+
+  for(let i = 1; i < tokens.length; ++i)
+  { await slack.postCommentInAuction(tokens[i]);
+  }
+
+  await slack.postCommentInAuction(`To bid, type \`bid xxx\` where \`xxx\` is the amount, e.g. \`bid 500\`. We'll start at *${minimum}*.`);
+
+  await monitorAuction(padded, parseInt(minimum));
+};
+
+const monitorAuction = (lot, minimum) =>
+{ return new Promise(async (resolve, reject) =>
+  { const highest =
+    { user: null
+    , bid: minimum
+    };
+
+    let open = true;
+
+    const endAuction = async () =>
+    { open = false;
+      timeoutID = null;
+      slack.setMessageCallback(null);
+
+      await slack.postCommentInAuction(`*SOLD* LOT *#${lot}* to <@${highest.user}> for *${highest.bid}*`);
+      resolve();
+    };
+
+    setTimeout(
+      async () =>
+      { if(highest.user == null)
+        { open = false;
+          slack.setMessageCallback(null);
+
+          await slack.postCommentInAuction('No one wanted this? More for me then!');
+          resolve();
+        }
+      }
+    , 120000
+    );
+
+    let timeoutID = null;
+
+    slack.setMessageCallback(async (event) =>
+    { const bids = event.text.match(/bid\s+?([0-9]+)\s?/ig);
+      if(bids)
+      { const bid = parseInt(bids[0].substring(4));
+        if(bid > highest.bid)
+        { if(!open)
+          { return;
+          }
+
+          highest.bid = bid;
+          highest.user = event.user;
+
+          timeoutID != null && clearTimeout(timeoutID);
+
+          await slack.postCommentInAuction(`Confirmed bid *${bid}* from <@${event.user}>`);
+
+          timeoutID = setTimeout(endAuction, 60000);
+        }
+      }
+    });
+  });
+}
--- slack.js
+++ slack.js
@@ -2,18 +2,34 @@ const { RTMClient } = require('@slack/rtm-api');
 const { WebClient } = require('@slack/web-api');
 
 const token = process.env.SLACK_BOT_TOKEN;
-const channel = process.env.SLACK_CHANNEL;
+const channel =
+{ general:
+  { code: process.env.SLACK_GENERAL_CHANNEL
+  , label: 'general'
+  }
+, auction:
+  { code: process.env.SLACK_AUCTION_CHANNEL
+  , label: 'auction'
+  }
+};
 
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
 
+let messageCallBack = null;
+
 const init = async () =>
 { const { self, team } = await rtm.start();
-}
+};
 
-const postLot = async (lot, photo, description) =>
+const typing = async (channel) =>
+{ await rtm.sendTyping(channel);
+};
+
+const postLotInGeneral = async (lot, photo) =>
 { try
-  { await rtm.sendTyping(channel);
+  { 
+    typing(channel.general.code);
 
     const result = await web.chat.postMessage(
     { blocks:
@@ -21,7 +37,7 @@ const postLot = async (lot, photo, description) =>
         { type: 'section'
         , text:
           { type: 'mrkdwn',
-            text: `Now bidding for *Lot #${lot}*.`
+            text: `Now bidding on LOT *#${lot}*.`
           }
         }
       , { type: 'image'
@@ -31,31 +47,108 @@ const postLot = async (lot, photo, description) =>
       , { type: 'section'
         , text:
           { type: 'mrkdwn',
-            text: description
+            text: 'Head on over to #haykos-auction to start bidding!'
           }
         }
-      , { type: 'divider'
-        }
       ]
-    , channel: channel
+    , channel: channel.general.code
     });
   }
   catch(e)
-  { console.log(`Error posting Lot #${lot}`);
+  { console.log(`Error posting LOT #${lot} in general`);
     console.log(e);
     console.log(e.data)
   }
-}
+};
+
+const postLot = async (lot, photo) =>
+{ try
+  { 
+    typing(channel.auction.code);
+
+    const result = await web.chat.postMessage(
+    { blocks:
+      [
+        { type: 'section'
+        , text:
+          { type: 'mrkdwn',
+            text: `Now bidding on LOT *#${lot}*.`
+          }
+        }
+      , { type: 'image'
+        , image_url: `http://drive.google.com/uc?export=view&id=${photo}`
+        , alt_text: '...'
+        }
+      ]
+    , channel: channel.auction.code
+    });
+  }
+  catch(e)
+  { console.log(`Error posting LOT #${lot} in auction`);
+    console.log(e);
+    console.log(e.data)
+  }
+};
+
+const postCommentInGeneral = async (comment) =>
+{ await postComment(channel.general, comment);
+};
+
+const postCommentInAuction = async (comment) =>
+{ await postComment(channel.auction, comment);
+};
+
+const postComment = async (channel, comment) =>
+{ try
+  { 
+    typing(channel.code);
+    await wait(comment);
+
+    const result = await web.chat.postMessage(
+    { blocks:
+      [
+        { type: 'section'
+        , text:
+          { type: 'mrkdwn',
+            text: comment
+          }
+        }
+      ]
+    , channel: channel.code
+    });
+  }
+  catch(e)
+  { console.log(`Error posting comment in ${channel.label}`);
+    console.log(e);
+    console.log(e.data)
+  }
+};
+
+const setMessageCallback = (callback) =>
+{ messageCallBack = callback
+};
 
 rtm.on('connected', (event) => {
   console.log('connected');
 });
 
 rtm.on('message', (event) => {
-  console.log(event);
+  messageCallBack
+  && event.channel == channel.auction.code
+  && messageCallBack(event);
 });
+
+const wait = (message) =>
+{ return new Promise((resolve, reject) =>
+  { setTimeout(resolve, (message.length * 50) + 1000);
+  });
+};
 
 module.exports = {
   init,
-  postLot
+  postLotInGeneral,
+  postLot,
+  postCommentInGeneral,
+  postCommentInAuction,
+  setMessageCallback
 }
